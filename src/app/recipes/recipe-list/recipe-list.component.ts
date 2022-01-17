40921a29ad3a525753921bcda3dd8becad569b3c
Michael Frank <mfrankwork@gmail.com>
2022-01-17 16:16:00 -0500

    Add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe(
+			'Biscuits',
+			'Perfect Homemade Biscuits',
+			'https://www.momontimeout.com/wp-content/uploads/2012/05/biscuit-recipe-easy-1.jpg'
+		),
+		new Recipe(
+			'Biscuits',
+			'Perfect Homemade Biscuits',
+			'https://www.momontimeout.com/wp-content/uploads/2012/05/biscuit-recipe-easy-1.jpg'
+		)
+	];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
