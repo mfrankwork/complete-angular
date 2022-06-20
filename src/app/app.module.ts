146004723d18348987208dea8abaa62fbf580d9b
Michael Frank <mfrankwork@gmail.com>
2022-06-20 12:48:17 -0400

    Enhanced dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
@@ -16,12 +16,15 @@ import { DropdownDirective } from './shared/dropdown.directive';
   declarations: [
     AppComponent,
     HeaderComponent,
+
     RecipesComponent,
-    RecipeListComponent,
     RecipeDetailComponent,
+    RecipeListComponent,
     RecipeItemComponent,
+
     ShoppingListComponent,
     ShoppingEditComponent,
+
     DropdownDirective
   ],
   imports: [BrowserModule, FormsModule],
